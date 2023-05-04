a1beb0336eb8ae0b96c76f6301fe678b2efafcf7
Rodrigo Castro <castro.rl@gmail.com>
2023-05-04 16:28:08 -0300

    created the context file, moved props and functions to it, I made the context available globaly, and called functions and props to inside UserResults component

--- src/components/shared/Spinner.jsx
+++ src/components/shared/Spinner.jsx
@@ -1,15 +1,14 @@
 import spinner from '../assets/spinner.gif';
 const Spinner = () => {
   return (
-    <img
-      src={spinner}
-      alt='Loading...'
-      style={{
-        width: '30px',
-        margin: 'auto',
-        display: 'block',
-      }}
-    ></img>
+    <div className='w-30 mt-20'>
+      <img 
+        className='text-center mx-auto'
+        width={180} 
+        src={spinner}
+        alt='Loading...'
+      ></img>
+    </div>
   );
 };
 
