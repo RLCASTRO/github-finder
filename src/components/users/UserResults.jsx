b775eef185ac22991a8b1c8087ace3837d8a290d
Rodrigo Castro <castro.rl@gmail.com>
2023-05-05 15:20:50 -0300

    Created the Alert, User, UserResults, UserSearch, and its Context and reducers

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,24 +1,17 @@
-import { useEffect, useState, useContext } from 'react';
+import { useContext } from 'react';
 import Spinner from '../shared/Spinner.jsx';
 import UserItem from '../users/UserItem.jsx';
-import GithubContext from '../../context/github/GitHubContext.js';
+import GithubContext from '../../context/github/GithubContext';
 
 const UserResults = () => {
-
-  const { users, loading, fetchUsers} = useContext(GithubContext);
-
-
-  useEffect(() => {
-    fetchUsers();
-  }, []);
-
+  const { users, loading } = useContext(GithubContext);
 
   return loading ? (
     <Spinner />
   ) : (
     <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>
       {users.map((user) => (
-          <UserItem key={user.id} user={user} />
+        <UserItem key={user.id} user={user} />
       ))}
     </div>
   );
